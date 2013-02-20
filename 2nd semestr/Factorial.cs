6b79cd985529a3edf94290a990a20fd0b683edf0
Timofeev Bogdan <timbog@mail.ru>
2013-02-20 18:28:56 +0400

    Factorial

---
+++ 2nd semestr/Factorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private static int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.Write("Enter number: ");
+            i = int.Parse(Console.ReadLine());
+            Console.Write("Factorial is : ");
+            Console.WriteLine(Factorial(i));
+        }
+    }
+}
