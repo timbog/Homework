4c2a78476a71494db8fd2e08f26e0102a67822b6
Timofeev Bogdan <timbog@mail.ru>
2013-02-20 18:37:05 +0400

    Fibonacci added

---
+++ 2nd semestr/Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        private static int Fib(int n)
+        {
+            if ((n == 1) || (n == 2))
+                return 1;
+            else
+                return Fib(n - 1) + Fib(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int i = int.Parse(Console.ReadLine());
+            Console.Write("Fibonacci is: ");
+            Console.WriteLine(Fib(i));
+        }
+    }
+}
