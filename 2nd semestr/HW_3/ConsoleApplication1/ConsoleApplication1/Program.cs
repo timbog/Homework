22065a3cc78a21934e95a228c07a1fc6a2f0f0f2
Timofeev Bogdan <timbog@mail.ru>
2013-02-28 19:26:02 +0400

    Stack added

---
+++ 2nd semestr/HW_3/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class CreateStack
+    {
+
+        private int size;
+
+        private int high = -1;
+
+        private int[] value;
+
+        public CreateStack(int size)
+        {
+                this.size = size;
+                value = new int[size];
+        }
+
+        public int Take()
+        {
+                return value[high - 1];
+        }
+ 
+        public void Put(int number)
+        {
+                value[high + 1] = number;
+        }
+ 
+        public int Top()
+        {
+                return value[high];
+        }    
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
