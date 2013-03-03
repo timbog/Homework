5d1bed673cbc7e5cf4d5f104eac6890eeeb15c6c
Timofeev Bogdan <timbog@mail.ru>
2013-03-03 20:20:59 +0400

    Stack changed to normal type

--- 2nd semestr/HW_3/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ 2nd semestr/HW_3/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,48 +6,83 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class CreateStack
+    class StackElement
     {
-
-        private int size;
-
-        private int high = -1;
-
-        private int[] value;
-
-        public CreateStack(int size)
+        public int data;
+        public StackElement next;
+        public StackElement(int a)
         {
-                this.size = size;
-                value = new int[size];
+            this.data = a;
+            this.next = null;
         }
-
-        public int Take()
+    }
+    class Stack
+    {
+        public StackElement head = null;
+        public void print()
         {
-                return value[high - 1];
-        }
- 
-        public void Put(int number)
-        {
-                value[high + 1] = number;
-        }
- 
-        public int Top()
-        {
-                return value[high];
-        }
-
-        public void Print()
-        {
-            for (int i = 0; i < size; ++i)
+            StackElement i = this.head;
+            if (i == null)
+                Console.WriteLine("Empty Stack");
+            while (i != null)
             {
-                Console.WriteLine(value[i]);
-        } 
-}
+                Console.WriteLine(i.data);
+                i = i.next;
+            }
+        }
 
+        public void add(int number)
+        {
+            StackElement i = this.head;
+            StackElement j = new StackElement(number);
+            j.next = i;
+            this.head = j;
+        }
+
+        public int take()
+        {
+            StackElement i = this.head;
+            if (i == null)
+            {
+                Console.WriteLine("Empty Stack");
+                return -1;
+            }
+            this.head = i.next;
+            return i.data;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Stack stack = new Stack();
+            Console.WriteLine("Enter 0 exit");
+            Console.WriteLine("Enter 1 to print the stack");
+            Console.WriteLine("Enter 2 to add a number to the stack");
+            Console.WriteLine("Enter 3 to take a number from the stack");
+            int command = 1;
+            int num = 0;
+            while (command != 0)
+            {
+                Console.WriteLine("Enter operation number: ");
+                command = int.Parse(Console.ReadLine());
+                if (command == 1)
+                {
+                    Console.WriteLine("Stack is");
+                    stack.print();
+                }
+                if (command == 2)
+                {
+                    Console.WriteLine("Enter number to add: ");
+                    num = int.Parse(Console.ReadLine());
+                    stack.add(num);
+                }
+                if (command == 3)
+                {
+                    Console.WriteLine(stack.take());
+                }
+            }
         }
     }
 }
