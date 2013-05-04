8f3a3f87744c5c6c8a1237d61618bfdfb98a1294
timbog <timbog@mail.ru>
2013-05-04 11:31:10 +0400

    ChangeList added

---
+++ 2nd semestr/HW_6/ChangeList/ChangeList.Tests/ChangeListTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+namespace ChangeList.Tests
+{
+    [TestClass]
+    public class ChangeListTests
+    {
+        [TestMethod]
+        public void MapTest()
+        {
+            List<int> list = Change.Map(new List<int>() { 1, 2, 3 }, x => x * 2);
+            Assert.IsTrue(list.Contains(2));
+            Assert.IsTrue(list.Contains(4));
+            Assert.IsTrue(list.Contains(6));
+        }
+    }
+}
---
+++ 2nd semestr/HW_6/ChangeList/ChangeList/Change.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeList
+{
+    public class Change
+    {
+        /// <summary>
+        /// Changing function
+        /// </summary>
+        /// <param name="list">List to change</param>
+        /// <param name="function">Operation to do</param>
+        /// <returns>Changed list</returns>
+        public static List <int> Map(List <int> list, Func <int, int> function)
+        {
+            List <int> tmp = new List <int>();
+            for (int i = 0; i < list.Count(); ++i)
+            {
+                tmp.Add(function(list[i]));
+            }
+            return tmp;
+        }
+    }
+}
