9462fc9735a8c33dc19e3715fe3f8429d51682c5
timbog <timbog@mail.ru>
2013-05-06 21:30:03 +0400

    6-2 added

---
+++ 2nd semestr/HW_6/BoolList/BoolList.Tests/BoolListTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BoolList.Tests
+{
+    [TestClass]
+    public class BoolListTests
+    {
+        [TestMethod]
+        public void FilterTest()
+        {
+            List<int> list = new List<int>() { 1, 2, 3 };
+            List<int> list1 = Change.Filter(list, x => x == 2);
+            Assert.AreEqual(list1[0], 2);
+            Assert.AreEqual(list1.Count, 1);
+        }
+    }
+}
---
+++ 2nd semestr/HW_6/BoolList/BoolList/Change.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoolList
+{
+    public class Change
+    {
+        /// <summary>
+        /// Filter method
+        /// </summary>
+        /// <param name="list">List</param>
+        /// <param name="function">Bool Function with int parameter</param>
+        /// <returns>Changed with this function list</returns>
+        public static List<int> Filter(List<int> list, Func<int, bool> function)
+        {
+            List<int> tmp = new List<int>();
+            for (int i = 0; i < list.Count(); ++i)
+            {
+                if (function(list[i]))
+                {
+                    tmp.Add(list[i]);
+                }
+            }
+            return tmp;
+        }
+    }
+}
