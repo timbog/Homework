a4a887c398cb2764665c9367ded1627d406efc41
timbog <timbog@mail.ru>
2013-05-07 15:12:27 +0400

    FoldFunction(6-3) added

---
+++ 2nd semestr/HW_6/FoldFunction/Fold.Tests/FoldTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+namespace Fold.Tests
+{
+    [TestClass]
+    public class FoldTests
+    {
+        [TestMethod]
+        public void FoldTest()
+        {
+            Assert.AreEqual(FoldFunction.Change.Fold(new List<int>() { 1, 2, 3 }, 1, (acc, elem) => acc * elem), 6);
+        }
+    }
+}
---
+++ 2nd semestr/HW_6/FoldFunction/FoldFunction/Change.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoldFunction
+{
+    public class Change
+    {
+        /// <summary>
+        /// Fold function
+        /// </summary>
+        /// <param name="list">List</param>
+        /// <param name="value">Start value</param>
+        /// <param name="function">Changing function</param>
+        /// <returns>Integer result of Fold function</returns>
+        public static int Fold(List<int> list, int value, Func<int, int, int> function)
+        {
+            for (int i = 0; i < list.Count; ++i)
+            {
+                value = function(value, list[i]);
+            }
+            return value;
+        }
+    }
+}
