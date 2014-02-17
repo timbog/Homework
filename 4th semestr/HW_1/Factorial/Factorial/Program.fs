df5f8980294828ce475cb7454d9c89bdda4d4e32
Timofeev Bogdan <timbog@mail.ru>
2014-02-17 18:09:13 +0400

    hw added

---
+++ 4th semestr/HW_1/Factorial/Factorial/Program.fs
@@ -0,0 +1,11 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+// Дополнительную справку см. в проекте "Учебник по F#".
+let rec fac n =
+    if n < 2 then 1
+    else n * fac(n - 1)
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    let x = System.Console.Read() - 48;
+    System.Console.WriteLine(fac x);
+    0 // возвращение целочисленного кода выхода
---
+++ 4th semestr/HW_1/Fibonacci/Fibonacci/Program.fs
@@ -0,0 +1,12 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+// Дополнительную справку см. в проекте "Учебник по F#".
+let rec fib n =
+    if n = 1 then 1
+    else if n = 2 then 2
+    else fib(n - 1) + fib(n - 2)
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    let x = System.Console.Read() - 48;
+    System.Console.WriteLine(fib 4);
+    0 // возвращение целочисленного кода выхода
