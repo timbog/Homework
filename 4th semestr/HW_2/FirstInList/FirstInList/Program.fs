77544496468dbf49e93ffee8ac5d2a8bfb53af38
Timofeev Bogdan <timbog@mail.ru>
2014-02-26 16:41:30 +0400

    hw added

--- 4th semestr/HW_2/FirstInList/FirstInList/Program.fs
+++ 4th semestr/HW_2/FirstInList/FirstInList/Program.fs
@@ -1,19 +1,15 @@
 ﻿System.Console.WriteLine("Press x to stop adding values to list")
 let rec buildList list =
     let input = System.Console.ReadLine()
-    if (input <>"x") then
-       let list = [System.Int32.Parse(input)] :: (list)
     match input with
         | "x" -> list
-        | _ -> buildList list
+        | _ -> buildList (list @ [System.Int32.Parse(input)])
 let list = buildList []
-printfn "Result is :%A" list
 System.Console.WriteLine("Enter number to find: ")
 let number = System.Int32.Parse(System.Console.ReadLine())
-
-//let rec findFunction iterator=
-//    match (list.Item(iterator) = number) with
-//    | true -> iterator
- //   | false -> findFunction (iterator + 1)
-//let output = findFunction 0;
-//printfn "Result is :%A" output
+let rec findFunction iterator=
+    match (list.Item(iterator) = number) with
+    | true -> iterator
+    | false -> findFunction (iterator + 1)
+let output = findFunction 0;
+printfn "Result is :%A" output
---
+++ 4th semestr/HW_2/Palindrome/Palindrome/Program.fs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Enter string: ")
+let input = System.Console.ReadLine();
+
+let palCheck (str:string) =
+    match str.[0] with
+    |str.[str.Length] when (str.Length <> (1||0))-> palCheck str.[1..str.Length - 1]
+    |str.[str.Length] when (str.Length = (1||0)) ->true
+    |_ -> false
+let check = palCheck input
+if check then
+    System.Console.WriteLine("palindrome")
+ else
+     System.Console.WriteLine("no")
+
+
+
