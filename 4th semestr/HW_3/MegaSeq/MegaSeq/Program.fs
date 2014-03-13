7252ffbf91996f483c54783e88601dd84997e0f4
Timofeev Bogdan <timbog@mail.ru>
2014-03-13 20:24:55 +0400

    some more hw added

---
+++ 4th semestr/HW_3/MegaSeq/MegaSeq/Program.fs
@@ -0,0 +1,16 @@
+﻿let rec simpleCheck value temp= //function which checks if the value is simple
+    match (value % temp) with
+    |0  when value<>temp -> false
+    |_ when temp > 1 -> simpleCheck value (temp - 1)
+    |_ -> true
+
+let rec simpleNumber i = //function which returns the nearest simple value
+    match simpleCheck i i with
+    |true -> i
+    |false-> simpleNumber i + 1
+
+let sequence = Seq.initInfinite(fun x -> simpleNumber x)
+
+
+
+    
