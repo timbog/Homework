c931185a2c65b43c90082dcc8779fef6945d4e36
Timofeev Bogdan <timbog@mail.ru>
2014-03-16 13:42:04 +0400

    Brackets added

---
+++ 4th semestr/HW_4/Brackets/Brackets/Program.fs
@@ -0,0 +1,16 @@
+﻿printfn "Enter string"
+let input = System.Console.ReadLine()
+let ifCorrect = //function which returns if sequence is correct
+    let rec correctBracket list iterator =
+        match input.[iterator] with
+        | '{' | '(' | '[' when iterator <> input.Length - 1 -> correctBracket (input.[iterator] :: list) (iterator + 1)
+        | '}' when list.Head = '{' && iterator <> input.Length - 1 -> correctBracket (list.Tail) (iterator + 1)
+        | ']' when list.Head = '[' && iterator <> input.Length - 1 -> correctBracket (list.Tail) (iterator + 1)
+        | ')' when list.Head = '(' && iterator <> input.Length - 1 -> correctBracket (list.Tail) (iterator + 1)
+        | '}' when list.Head = '{' && (list.Length = 1) && iterator = input.Length - 1 -> true
+        | ']' when list.Head = '[' && (list.Length = 1) && iterator = input.Length - 1 -> true
+        | ')' when list.Head = '(' && (list.Length = 1) && iterator = input.Length - 1 -> true
+        | _-> false
+    correctBracket [] 0
+printfn "Result is: %A" ifCorrect
+     
