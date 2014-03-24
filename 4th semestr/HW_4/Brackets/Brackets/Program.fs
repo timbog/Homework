43bc6b33a2d5ca0a9b40f40e75edc51173b663f5
timbog <timbog@mail.ru>
2014-03-24 18:53:36 +0400

    brackets changed

--- 4th semestr/HW_4/Brackets/Brackets/Program.fs
+++ 4th semestr/HW_4/Brackets/Brackets/Program.fs
@@ -1,19 +1,22 @@
 ﻿printfn "Enter string"
 let input = System.Console.ReadLine()
+
+let opposite bracket = 
+    match bracket with
+    |'}' ->'{'
+    |']' ->'['
+    |')' ->'('
+    |_ ->failwith "Not bracket"
+
 let ifCorrect = //function which returns if sequence is correct
     let rec correctBracket list iterator =
         match input.[iterator] with
         | '{' | '(' | '[' when iterator <> input.Length - 1 -> correctBracket (input.[iterator] :: list) (iterator + 1)
-        | '}' when list.Head = '{' && iterator <> input.Length - 1 -> correctBracket (list.Tail) (iterator + 1)
-        | ']' when list.Head = '[' && iterator <> input.Length - 1 -> correctBracket (list.Tail) (iterator + 1)
-        | ')' when list.Head = '(' && iterator <> input.Length - 1 -> correctBracket (list.Tail) (iterator + 1)
-        | '}' when list.Head = '{' && (list.Length = 1) && iterator = input.Length - 1 -> true
-        | ']' when list.Head = '[' && (list.Length = 1) && iterator = input.Length - 1 -> true
-        | ')' when list.Head = '(' && (list.Length = 1) && iterator = input.Length - 1 -> true
+        |  bracket when ((bracket ='}') || (bracket =')') || (bracket =']')) && (iterator <> input.Length - 1) && (list.Head = opposite bracket)->  correctBracket (list.Tail) (iterator + 1)
+        |  bracket when ((bracket ='}') || (bracket =')') || (bracket =']')) && (list.Head = opposite bracket) && (list.Length = 1) && (iterator = input.Length - 1) -> true
         | '{' | '(' | '[' |'}' | ')' | ']' -> false
         | _ when iterator <> input.Length - 1 -> correctBracket list (iterator + 1)
         | _ when iterator = input.Length - 1 && list.Length = 0 -> true
         |_ -> false
     correctBracket [] 0
 printfn "Result is: %A" ifCorrect
-     
