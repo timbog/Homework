ec03eedba320c1c1911af8479f0c57b953d3aa9d
timbog <timbog@mail.ru>
2014-04-08 16:15:09 +0400

    2nd task from controlWork added

---
+++ 4th semestr/Control_1/TheBiggest/TheBiggest/Program.fs
@@ -0,0 +1,26 @@
+﻿let palCheck number = //funcion which checks if number is a palindome
+    let rec degreeTen number result iterator =
+        match iterator with
+        |temp when (temp < number) ->degreeTen number (result * 10) (iterator + 1)
+        |_-> result
+    let rec revert number list =
+        match number with
+        |0 -> list
+        |_-> revert (number / 10) ((number % 10) :: list)
+    let rec build (list:List<int>) iterator num =
+        match iterator with
+        |temp when (temp = list.Length) -> num
+        |_->build list (iterator + 1) (num + list.Item(iterator) * degreeTen (iterator) 1 0)
+    (number = build (revert number []) 0 0)
+
+let biggest = //function which finds the biggest number
+    let rec big iter1 iter2 max =
+        match iter2 with
+            |_ when (iter1 = 1000) -> max
+            |temp when (temp < 1000) -> match (palCheck (iter2 * iter1)) with
+                                            |true when (iter2 * iter1 > max) -> big iter1 (iter2 + 1) (iter2 * iter1)
+                                            |_ -> big iter1 (iter2 + 1) max
+            |temp when (temp = 1000) -> big (iter1 + 1) 100 max
+            |_->failwith"Out of range"
+    big 100 100 0
+printfn "Result is:%A" biggest
---
+++ 4th semestr/HW_5/LocalNetwork/LocalNetwork/Program.fs
@@ -0,0 +1,24 @@
+﻿type Network (osMatrix:List<string>, virusMatrix:List<bool>, probabilityMatrix:List<int>) = 
+    let LinkMatrix = Array2D.init 3 3 (fun i j -> (i + j + 1) % 2)
+    let SystemMatrix = osMatrix
+    let VirusMatrix = virusMatrix
+    let ProbabilityMatrix = probabilityMatrix
+    member n.OneMove() =
+        let newInfection = Array.init (LinkMatrix.GetLength(0)) (fun x -> false)
+        for i in 0..LinkMatrix.GetLength(0) do           
+            for j in 0..LinkMatrix.GetLength(0) do
+                if ((LinkMatrix.[i, j] = 1) && (VirusMatrix.Item(j)) && not (VirusMatrix.Item(i)) && not (newInfection.[i])) then
+                    let random = (new System.Random()).Next(0, 1000)
+                    match SystemMatrix.Item(i) with
+                    |"Windows" -> match ((random % 5 == 1) || (random % 5 == 0)) with
+                                  |true-> VirusMatrix.Item(i) <- true
+                                  |false->VirusMatrix[i] <- false
+                    |"Linux" -> match (random % 5 == 3) with
+                                  |true-> VirusMatrix[i] <- true
+                                  |false->n.VirusMatrix[i] <- false
+                    |"MacOS" -> match ((random % 5 == 1) || (random % 5 == 0) || (random % 5 == 3)) with
+                                  |true-> VirusMatrix[i] <- true
+                                  |false->VirusMatrix[i] <- false
+                    if (VirusMatrix[i]) then
+                        newInfection[i] = true
+
