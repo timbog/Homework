e5226105eebc00d1ee883ddd403894de9502618b
timbog <timbog@mail.ru>
2014-04-22 10:12:27 +0400

    brackets changed

--- 4th semestr/HW_4/Brackets/Brackets/Program.fs
+++ 4th semestr/HW_4/Brackets/Brackets/Program.fs
@@ -8,12 +8,17 @@ let opposite bracket =
     |')' ->'('
     |_ ->failwith "Not bracket"
 
+let isCloseBracket str (list:List<char>) =
+    match str with
+    |('}' | ')' | ']') when (list.Head = opposite str) -> true
+    |_ -> false
+
 let ifCorrect = //function which returns if sequence is correct
     let rec correctBracket list iterator =
         match input.[iterator] with
         | '{' | '(' | '[' when iterator <> input.Length - 1 -> correctBracket (input.[iterator] :: list) (iterator + 1)
-        |  bracket when ((bracket ='}') || (bracket =')') || (bracket =']')) && (iterator <> input.Length - 1) && (list.Head = opposite bracket)->  correctBracket (list.Tail) (iterator + 1)
-        |  bracket when ((bracket ='}') || (bracket =')') || (bracket =']')) && (list.Head = opposite bracket) && (list.Length = 1) && (iterator = input.Length - 1) -> true
+        |  bracket when (isCloseBracket bracket list) && (iterator <> input.Length - 1)->  correctBracket (list.Tail) (iterator + 1)
+        |  bracket when (isCloseBracket bracket list) && (iterator = input.Length - 1) && (list.Length = 1)  -> true
         | '{' | '(' | '[' |'}' | ')' | ']' -> false
         | _ when iterator <> input.Length - 1 -> correctBracket list (iterator + 1)
         | _ when iterator = input.Length - 1 && list.Length = 0 -> true
