fc484842733730d5071bfd359641fce3b43bf3b8
timbog <timbog@mail.ru>
2014-04-22 09:57:10 +0400

    megaseq changed

--- 4th semestr/HW_5/LocalNetwork/LocalNetwork/Program.fs
+++ 4th semestr/HW_5/LocalNetwork/LocalNetwork/Program.fs
@@ -1,24 +1,65 @@
-﻿type Network (osMatrix:List<string>, virusMatrix:List<bool>, probabilityMatrix:List<int>) = 
+﻿type Network() = 
     let LinkMatrix = Array2D.init 3 3 (fun i j -> (i + j + 1) % 2)
-    let SystemMatrix = osMatrix
-    let VirusMatrix = virusMatrix
-    let ProbabilityMatrix = probabilityMatrix
+    let SystemMatrix = [|"Windows"; "MacOS"; "Linux"|]
+    let VirusMatrix = Array.init 3 (fun i -> (i + 2 % 2) = 1)
     member n.OneMove() =
         let newInfection = Array.init (LinkMatrix.GetLength(0)) (fun x -> false)
         for i in 0..LinkMatrix.GetLength(0) do           
             for j in 0..LinkMatrix.GetLength(0) do
-                if ((LinkMatrix.[i, j] = 1) && (VirusMatrix.Item(j)) && not (VirusMatrix.Item(i)) && not (newInfection.[i])) then
+                if ((LinkMatrix.[i, j] = 1) && (VirusMatrix.[j])) && not ((VirusMatrix.[i]) && not (newInfection.[i])) then
                     let random = (new System.Random()).Next(0, 1000)
-                    match SystemMatrix.Item(i) with
-                    |"Windows" -> match ((random % 5 == 1) || (random % 5 == 0)) with
-                                  |true-> VirusMatrix.Item(i) <- true
-                                  |false->VirusMatrix[i] <- false
-                    |"Linux" -> match (random % 5 == 3) with
-                                  |true-> VirusMatrix[i] <- true
-                                  |false->n.VirusMatrix[i] <- false
-                    |"MacOS" -> match ((random % 5 == 1) || (random % 5 == 0) || (random % 5 == 3)) with
-                                  |true-> VirusMatrix[i] <- true
-                                  |false->VirusMatrix[i] <- false
-                    if (VirusMatrix[i]) then
-                        newInfection[i] = true
+                    match SystemMatrix.[i] with
+                    |"Windows" -> match ((random % 5 = 1) || (random % 5 = 0)) with
+                                  |true-> VirusMatrix.[i] <- true
+                                  |false->VirusMatrix.[i] <- false
+                    |"Linux" -> match (random % 5 = 3) with
+                                  |true-> VirusMatrix.[i] <- true
+                                  |false -> VirusMatrix.[i] <- false
+                    |"MacOS" -> match ((random % 5 = 1) || (random % 5 = 0) || (random % 5 = 3)) with
+                                  |true-> VirusMatrix.[i] <- true
+                                  |false -> VirusMatrix.[i] <- false
+                    |_-> failwith "No such system"
+                    if (VirusMatrix.[i]) then
+                        newInfection.[i] <- true
+    member n.GetState() =
+        let y = n.OneMove()
+        VirusMatrix
+
+    member n.AllInfected() =
+        let rec check iterator =
+            match (VirusMatrix.[iterator]) with
+                |true when (iterator < VirusMatrix.Length - 1)-> check (iterator + 1)
+                |true when (iterator = VirusMatrix.Length - 1)-> true
+                |_->false
+        check 0
+    member n.ConnectionMatrix = LinkMatrix
+    member n.OsMatrix = SystemMatrix
+    member n.DisMatrix = VirusMatrix
+
+let network = new Network()
+let rec State()=
+            let rec printRes iterator =
+                match (iterator < network.DisMatrix.Length) with
+                    |true ->
+                             let state = (network .DisMatrix.[iterator].ToString())
+                             printfn "%A" (network.OsMatrix.[iterator] + state)
+                             printRes (iterator + 1)
+                    |false -> printfn "%A" ""
+            printRes 0
+
+let rec work() =
+    match network.AllInfected() with
+        |true-> printfn "All computers are infected"
+        |false-> 
+            let x = network.GetState
+            State()
+            Thread.Sleep(5000);
+            work()
+let temp = work()
+
+                        
+
+
+
+
 
