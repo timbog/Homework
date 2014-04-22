5184d4a1db54a17558ed07e9334e25953dbefeae
timbog <timbog@mail.ru>
2014-04-22 15:52:15 +0400

    Stack added

---
+++ 4th semestr/Control_1/Stack/Stack/Program.fs
@@ -0,0 +1,17 @@
+﻿type Stack() = //Stack
+    let mutable list = []
+    member s.Push(value) = list<-(value::list)
+    member s.Pop = match list with
+                    |[] -> failwith "Empty Stack"
+                    |_->
+                        let temp = list.Head
+                        list<-list.Tail;
+                        temp
+    member s.isEmpty() = list.IsEmpty
+
+let stack = new Stack()
+stack.Push(5)
+stack.Push(6)
+let y = stack.Pop
+let u = stack.isEmpty()
+printfn"%A" u
