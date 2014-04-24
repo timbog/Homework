ca3199abea109d0ffef5234ec142c8b5247bbc75
timbog <timbog@mail.ru>
2014-04-24 16:27:08 +0400

    treeMap added

--- 4th semestr/HW_4/TreeMap/TreeMap/Program.fs
+++ 4th semestr/HW_4/TreeMap/TreeMap/Program.fs
@@ -1,1 +1,10 @@
-﻿
+﻿type Tree<'a> = //tree
+    | Tree of 'a * Tree<'a> * Tree<'a>
+    | Tip of 'a
+
+let rec map(change:'a -> 'a) tree = //treeMap
+    match tree with
+        |Tree.Tree(temp, tree1, tree2) -> Tree.Tree(change temp, map change tree1, map change tree2)
+        |Tree.Tip(temp) -> Tree.Tip(change temp)
+
+
