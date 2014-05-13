340595069b09a76c35687c739ffdce18544e3c31
timbog <timbog@mail.ru>
2014-05-13 18:07:18 +0400

    workFlow added

---
+++ 4th semestr/HW_6/Worflow/Worflow/Program.fs
@@ -0,0 +1,10 @@
+﻿type Rounding(accuracy : int) =
+    member this.Bind ((x : float), (rest : float -> float)) = System.Math.Round(rest x , accuracy)
+    member this.Return (x : float) = System.Math.Round(x, accuracy)
+
+let result = Rounding 3 {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    }
+printfn "%A" result
