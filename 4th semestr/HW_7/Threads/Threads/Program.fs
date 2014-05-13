ceb6e84767958c5526dcbb5ba45e12b8ca7515ec
timbog <timbog@mail.ru>
2014-05-13 21:15:37 +0400

    threads added

---
+++ 4th semestr/HW_7/Threads/Threads/Program.fs
@@ -0,0 +1,18 @@
+﻿let array = [| for i in 1 .. 1000000 -> i |]
+
+let sumOfArray = 
+        let threadArray = [| for i in 1 .. 100 ->  new  System.ComponentModel.BackgroundWorker()|]
+        let result = ref 0
+        for i in 0..99 do
+            threadArray.[i].DoWork.Add(fun args ->
+                let array2 = Array.filter(fun x -> (x > 10000 * i) && (x <= 10000 * (i + 1))) array
+                let res = Array.sum array2
+                args.Result <- box res)
+            threadArray.[i].RunWorkerCompleted.Add(fun args ->
+                result := !result +  unbox args.Result)
+        for i in 0..99 do
+            threadArray.[i].RunWorkerAsync()
+        result.Value
+
+printfn "%A" (sumOfArray)
+
