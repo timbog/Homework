bb6ab93225994a75ecfc490ce2ba2bb142d18d5b
timbog <timbog@mail.ru>
2014-05-26 13:48:13 +0400

    threads changed again

--- 4th semestr/HW_7/Threads/Threads/Program.fs
+++ 4th semestr/HW_7/Threads/Threads/Program.fs
@@ -1,5 +1,5 @@
 ﻿let array = [| for i in 1 .. 1000000 -> 1 |]
-let sumOfArray = 
+let sumOfArray = //sum of array
         let threadArray = [| for i in 1 .. 100 ->  new  System.ComponentModel.BackgroundWorker()|]
         let result = ref 0
         for i in 0..99 do
@@ -10,6 +10,10 @@ let sumOfArray =
                 result := !result +  unbox args.Result)
         for i in 0..99 do
             threadArray.[i].RunWorkerAsync()
+        let mutable workArray = [| for i in 0 .. 99 -> threadArray.[i].IsBusy |]
+        printfn "%A" workArray
+        while ((Array.tryFind (fun x -> x = true) workArray) <> None) do
+            workArray <- [| for i in 0 .. 99 -> threadArray.[i].IsBusy |]                    
         result.Value
 
 printfn "%A" (sumOfArray)
