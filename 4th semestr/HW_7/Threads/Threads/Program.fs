a75e196af1f335b23116e5d4e85336c71c5097f4
timbog <timbog@mail.ru>
2014-05-26 11:26:18 +0400

    threads changed

--- 4th semestr/HW_7/Threads/Threads/Program.fs
+++ 4th semestr/HW_7/Threads/Threads/Program.fs
@@ -1,12 +1,10 @@
-﻿let array = [| for i in 1 .. 1000000 -> i |]
-
+﻿let array = [| for i in 1 .. 1000000 -> 1 |]
 let sumOfArray = 
         let threadArray = [| for i in 1 .. 100 ->  new  System.ComponentModel.BackgroundWorker()|]
         let result = ref 0
         for i in 0..99 do
-            threadArray.[i].DoWork.Add(fun args ->
-                let array2 = Array.filter(fun x -> (x > 10000 * i) && (x <= 10000 * (i + 1))) array
-                let res = Array.sum array2
+            threadArray.[i].DoWork.Add(fun args -> 
+                let res = Array.sum (Array.sub array i 10000)       
                 args.Result <- box res)
             threadArray.[i].RunWorkerCompleted.Add(fun args ->
                 result := !result +  unbox args.Result)
